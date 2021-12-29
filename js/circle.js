6006128d8798dbfad255fc994046fda87881e454
helloshantanu <jain.shantanu@outlook.com>
2021-12-29 12:53:05 +0530

    Changes

---
+++ js/circle.js
@@ -0,0 +1,14 @@
+
+
+var $circle = $('.circle');
+
+function moveCircle(e) {
+	TweenLite.to($circle, 0.3, {
+    css: {
+      left: e.pageX,
+      top: e.pageY
+    }
+  });
+}
+
+$(window).on('mousemove', moveCircle);
